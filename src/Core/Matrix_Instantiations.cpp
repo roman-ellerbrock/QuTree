706adf90abefd60671dd1a80668f4c62a4549995
romanellerbrock <romanellerbrock@gmail.com>
2020-04-17 00:01:31 +0200

    Added missing instantiation to matrix class.

--- src/Core/Matrix_Instantiations.cpp
+++ src/Core/Matrix_Instantiations.cpp
@@ -31,6 +31,7 @@ template istream& operator>> <cd> (istream& is, Matrixcd& A);
 typedef double doub;
 template Vector<double> multAB<doub>(const Matrix<double>& A,
 	const Vector<double>& B);
+template Matrix<double> multAB(const Matrix<double>& A, const Matrix<double>& B);
 template Matrix<doub> addAB(const Matrix<doub>& A, const Matrix<doub>& B);
 template Matrix<cd> multscalar<cd, doub>(const double sca,
 	const Matrix<cd>& B);
