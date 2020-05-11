f9749e372af835c7d1b7b8b0681046e41b72f252
Jason Ford <jeford@stanford.edu>
2020-05-11 10:42:55 -0700

    Add Matrix<double> subtraction

--- src/Core/Matrix_Instantiations.cpp
+++ src/Core/Matrix_Instantiations.cpp
@@ -34,6 +34,7 @@ template Vector<double> multAB<doub>(const Matrix<double>& A,
 	const Vector<double>& B);
 template Matrix<double> multAB(const Matrix<double>& A, const Matrix<double>& B);
 template Matrix<doub> addAB(const Matrix<doub>& A, const Matrix<doub>& B);
+template Matrix<doub> substAB(const Matrix<doub>& A, const Matrix<doub>& B);
 template Matrix<cd> multscalar<cd, doub>(const double sca,
 	const Matrix<cd>& B);
 template Matrix<doub> multscalar<doub, doub>(const double sca,
