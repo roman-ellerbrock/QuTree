5a4ec9d07d34aca538822dda702cf4dbdf2f9e9e
Roman Ellerbrock <romanellerbrock@gmail.com>
2022-07-07 18:42:54 -0700

    cleaner cuda support.

---
+++ cmake/cuda.cmake
@@ -0,0 +1,57 @@
+
+
+
+
+#####################################################################
+# CUDA support
+#####################################################################
+
+option (CUDA_FEATS "Set to On to use CUDA features" ON)
+
+if (CUDA_FEATS)
+	message("CUDA compilation enabled.")
+
+	enable_language(CUDA)
+	find_package(CUDA REQUIRED)
+
+	# Set CUDA_ARCHITECTURES
+	cmake_policy(SET "CMP0104" NEW)
+	if(NOT DEFINED ${CMAKE_CUDA_ARCHITECTURES})
+		set(CMAKE_CUDA_ARCHITECTURES 52 61 75)
+	endif()
+
+	set_target_properties(QuTree PROPERTIES 
+		CUDA_SEPARABLE_COMPILATION ON
+		)
+
+	set_target_properties(QuTree PROPERTIES 
+		IMPORTED_LOCATION "/usr/local/cuda/lib64/"
+		INTERFACE_INCLUDE_DIRECTORIES "/usr/local/cuda/include"
+		CUDA_ARCHITECTURES ${CMAKE_CUDA_ARCHITECTURES}
+	)
+
+	target_link_options(QuTree PUBLIC ${CUDA_FLAGS})
+
+	find_library(CUBLAS
+	NAMES cublas
+	HINTS /usr/local/cuda/lib64
+	REQUIRED)
+
+	find_library(CURAND
+	NAMES curand
+	HINTS /usr/local/cuda/lib64
+	REQUIRED)
+	set(CUDA_FLAGS "${CUBLAS} ${CURAND}")
+
+	#	separate_arguments(CUDA_FLAGS UNIX_COMMAND "${CUDA_FLAGS}")
+
+
+
+	message("CUDA Flags: ${CUDA_FLAGS}")
+	message("CUDA Include directories: ${CUDA_INCLUDE_DIRS}")
+
+endif()
+
+
+
+
